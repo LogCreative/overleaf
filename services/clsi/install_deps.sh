62b679592ff17994cedcedafbcd93a1dd82f8758
LogCreative <logcreative@outlook.com>
2024-07-20 23:57:20 +0800

    feat: use mirror for update

--- services/clsi/install_deps.sh
+++ services/clsi/install_deps.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+sed -i 's/deb.debian.org/mirrors.cloud.tencent.com/g' /etc/apt/sources.list.d/debian.sources
+sed -i 's/security.debian.org/mirrors.cloud.tencent.com/g' /etc/apt/sources.list.d/debian.sources
+
 apt-get update
 
 apt-get install -y \
